56ab6e26e72d275c8905e940dc1b63ea602eccc1
Hisham Barakat <83568664+hishambarakat10@users.noreply.github.com>
2025-02-13 22:47:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def home():
 
 @app.route("/run_notebook")
 def run_notebook():
-    file_path = r"C:\Users\hisha\Documents\CSCI_4385\message.json"  # Update this path if needed
+    file_path = r"/home/capstone/cap_project/ARGUS-main/ARGUS/message.json"  # Update this path if needed
 
     df = extract_data(file_path)
     if df is None or df.empty:
