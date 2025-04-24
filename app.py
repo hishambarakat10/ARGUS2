64fb0a087ec8e9ccfe93b432449fd1b046029c6b
Hisham Barakat <83568664+hishambarakat10@users.noreply.github.com>
2025-04-24 04:56:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def load_initial_logs(file_path="/var/log/suricata/fast.log", count=10):
 
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'  # Replace with something secure
-VIRUSTOTAL_API_KEY = "111a10aea56259d602d50d583fbe32a130c4a3f1e8fe9b5e258eb2f184e211bf"  # Replace this with your actual key
+VIRUSTOTAL_API_KEY = ""  # Replace this with your actual key
 socketio = SocketIO(app, cors_allowed_origins="*")
-EMAIL_ADDRESS = "throwawayemail735144@gmail.com"
-EMAIL_PASSWORD = "iifvxgfzitannnsj"              # Generated via website
-EMAIL_RECEIVER = "throwawayemail735144@gmail.com" # 
+EMAIL_ADDRESS = ""
+EMAIL_PASSWORD = ""              # Generated via website
+EMAIL_RECEIVER = "" # 
 
 log_data = []
 classification_counts = {}
@@ -135,7 +135,7 @@ def chat_with_langchain_bot():
     if not user_input:
         return jsonify({"error": "No message provided"}), 400
 
-    host_chatbot_url = "http://192.168.1.1:5005/chat"  # Replace with your actual host IP
+    host_chatbot_url = "http://:5005/chat"  # Replace with your actual host IP
 
     try:
         response = requests.post(host_chatbot_url, json={"message": user_input}, timeout=250)
